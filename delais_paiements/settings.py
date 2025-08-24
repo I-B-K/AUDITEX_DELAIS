42e9a6dd6c40da45d0bb640fc5ff73b865d897a8
BOUYAQINE <77923002+IBK-dev@users.noreply.github.com>
2025-08-24 19:38:00 +0100

    vs

--- delais_paiements/settings.py
+++ delais_paiements/settings.py
@@ -108,6 +108,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# Le répertoire où `collectstatic` placera tous les fichiers statiques pour la production.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
